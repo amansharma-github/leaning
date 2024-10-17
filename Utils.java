e1ced0c6917bf2c5a8f495fa4a90bb62992db6ac
Aman Sharma <aman.sharma.bgs@gmail.com>
2024-10-18 02:13:36 +0530

    This is first

---
+++ Utils.java
@@ -0,0 +1,11 @@
+// Utils.java
+
+public class Utils {
+    public static void greet(String name) {
+        System.out.println("Hello, " + name + "!");
+    }
+    
+    public static void farewell(String name) {
+        System.out.println("Goodbye, " + name + "!");
+    }
+}
