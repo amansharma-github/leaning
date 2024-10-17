2636604739ff2c08dca9298a805c77bb34909843
Aman Sharma <aman.sharma.bgs@gmail.com>
2024-10-18 02:21:02 +0530

    this is second

--- Main.java
+++ Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to My Git Demo Java Project!");
         Utils.greet("World");
+
+        System.out.println("hello world");
     }
 }
